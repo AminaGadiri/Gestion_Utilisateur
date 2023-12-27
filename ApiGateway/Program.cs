9a1270b710eefabe4587ddfab1491961241cebdb
AminaGadiri <aminagadiri14444@gmail.com>
2023-12-27 16:47:19 +0100

    Add Api Gateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,34 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddControllers();
+//2
+builder.Services.AddOcelot();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+//3
+app.UseOcelot().Wait();
+
+
+app.Run();
